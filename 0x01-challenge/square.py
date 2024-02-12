e249368f0f8fd8de767e73b5dc430ebab76b88a2
ib-babs <babatundeibrahim426@gmail.com>
2024-02-12 19:56:36 +0100

    Initial commit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+Class model
+"""
+
 
 class square():
+    """Define class"""
     
     width = 0
-    height = 0
 
     
     def __init__(self, *args, **kwargs):
@@ -12,17 +16,18 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * 2
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        return (self.width ** 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
